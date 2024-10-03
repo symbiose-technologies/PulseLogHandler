55c8a76703f2bf86b0d2f7fe29ea0f819d35cd28
Ryan McKinney <rydamckinney@gmail.com>
2024-10-03 17:55:15 -0400

    Bump to fix compilation

--- Package.swift
+++ Package.swift
@@ -7,15 +7,15 @@ let package = Package(
     platforms: [
         .iOS(.v15),
         .tvOS(.v15),
-        .macOS(.v12),
-        .watchOS(.v8)
+        .macOS(.v13),
+        .watchOS(.v9),
     ],
     products: [
         .library(name: "PulseLogHandler", targets: ["PulseLogHandler"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.2"),
-        .package(url: "https://github.com/kean/pulse.git", from: "5.0.0")
+        .package(url: "https://github.com/kean/pulse.git", from: "5.1.0")
     ],
     targets: [
         .target(name: "PulseLogHandler", dependencies: [
